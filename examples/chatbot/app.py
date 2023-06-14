68d1340478aa30a395a654ee490f5a8f5f3ebda4
suhaoson <zhhuzhihao@aikucun.com>
2023-06-14 09:23:08 +0800

    LangChain&chatbot

--- examples/chatbot/app.py
+++ examples/chatbot/app.py
@@ -1,7 +1,58 @@
+import openai
+import os
 import gradio as gr
 
-def greet(name):
-    return "Hello " + name + "!!"
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch()
+class Conversation:
+    def __init__(self, prompt, num_of_round):
+        self.prompt = prompt
+        self.num_of_round = num_of_round
+        self.messages = []
+        self.messages.append({"role": "system", "content": self.prompt})
+
+    def ask(self, question):
+        try:
+            self.messages.append( {"role": "user", "content": question})
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=self.messages,
+                temperature=0.5,
+                max_tokens=2048,
+                top_p=1,
+            )
+        except Exception as e:
+            print(e)
+            return e
+
+        message = response["choices"][0]["message"]["content"]
+        self.messages.append({"role": "assistant", "content": message})
+        
+        if len(self.messages) > self.num_of_round*2 + 1:
+            del self.messages[1:3]
+        return message
+
+prompt = """你是一个中国厨师，用中文回答做菜的问题。你的回答需要满足以下要求:
+1. 你的回答必须是中文
+2. 回答限制在100个字以内"""
+
+conv = Conversation(prompt, 10)
+
+def answer(question, history = []):
+    history.append(question)
+    response = conv.ask(question)
+    history.append(response)
+    responses = [(u, b) for u,b in zip(history[::2], history[1::2])]
+    return responses, history
+
+with gr.Blocks(css="#chatbot{height:300px} .overflow-y-auto{height:500px}" ) as demo:
+    chatbot = gr.Chatbox(elem_id="chatbot")
+    state = gr.State([])
+
+    with gr.Row():
+        text = gr.Textbox(show_label=False, placeholder="Enter text and press enter").style(container=False)
+
+    text.submit(answer, [text, state], [chatbot, state])
+
+
+demo.launch()
